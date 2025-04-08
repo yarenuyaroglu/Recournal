5a2fa0272b9755a518ee39925987deb97af370bf
yarenuyaroglu <147250031+yarenuyaroglu@users.noreply.github.com>
2025-04-08 16:44:04 +0300

    Add files via upload
    Home Screen, Favorites Screen and TabBar added.

---
+++ Recournal/Services/NetworkManager.swift
@@ -0,0 +1,47 @@
+//
+//  NetworkingManager.swift
+//  Recournal
+//
+//  Created by YarenEteration on 7.04.2025.
+//
+
+import Foundation
+import Combine
+
+
+
+
+//Network Manager
+class NetworkManager{
+    
+    //singleton örneği
+    static let shared = NetworkManager()
+    
+    let urlString = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+
+    
+    private init() { }
+    
+    
+    //Apiden rastgele tarifleri çekmek için Combine publisher fonksiyonu
+    func fetchMeals() -> AnyPublisher<[Meal], Error> {
+        
+        
+        let urlString = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+        guard let url = URL(string: urlString) else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        //URLSession ile veri çekip, decode ediyoruz.
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map{ $0.data}
+            .decode(type: MealResponse.self, decoder: JSONDecoder())
+            .map{ $0.meals}
+            .receive(on: DispatchQueue.main) //UI güncellemeleri için main thread
+            .eraseToAnyPublisher()
+    }
+}
+    
+    
+    
+
